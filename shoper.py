03d9193312e1b5e2df23bab0f028dd25ca151b31
dinukamadushan0 <dinukamadhushan513@gmail.com>
2024-08-25 15:46:12 +0530

    load, preprocess and binarize image

--- shoper.py
+++ shoper.py
@@ -2,3 +2,15 @@ import cv2
 from matplotlib import pyplot as plt
 import pytesseract
 import os
+
+def load_image(filepath):
+    image = cv2.imread(filepath)
+    return image
+
+
+def preprocess_image(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Binarization
+    _, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
+    return binary
